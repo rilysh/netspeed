e59903ae0de6b6a7703c621ac2fb410b39fb4309
rilysh <luhamailbox@gmail.com>
2022-11-02 00:41:32 +0700

    fix: check command before build

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/env sh
 
-if [ ! gccgo ]; then
+arch=$ARCHFLAGS | sed -E 's/-arch|[ ,]//g'
+
+if [ ! -x "$(command -v gccgo)" ]; then
     echo "GCCGo doesn't seems to be installed or in PATH. Falling back with default go compiler"
-    if [ ! go ]; then
+    if [ ! -x "$(command -v go)" ]; then
         echo "Go isn't installed in your system."
         exit
     fi
-    go build
+    env GOARCH=$arch go build
 else
     gccgo *.go -O2 -march=native -s -o netspeed
 fi
